80481b0f3cda370ae9c7a91503af7495d95c11a0
rubaiiyat <abirr1516@gmail.com>
2025-01-03 22:59:25 +0600

    change class to className in book component

--- src/Components/Book/Book.jsx
+++ src/Components/Book/Book.jsx
@@ -6,9 +6,9 @@ const Book = ({ book }) => {
   return (
     <div className="mt-10">
       <Link to={`/bookDetails/${bookId}`}>
-        <section class="p-8  bg-base-200    rounded-2xl min-h-[670px] flex-grow">
+        <section className="p-8  bg-base-200    rounded-2xl min-h-[670px] flex-grow">
           <img className="rounded-2xl" src={image} alt="" />
-          <div class="space-x-10 flex  mt-10">
+          <div className="space-x-10 flex  mt-10">
             {tags.map((tag, index) => (
               <p
                 key={index}
@@ -18,10 +18,10 @@ const Book = ({ book }) => {
               </p>
             ))}
           </div>
-          <h1 class="text-3xl my-5 text-start font-bold text-white">
+          <h1 className="text-3xl my-5 text-start font-bold text-white">
             {bookName}
           </h1>
-          <p class="mb-5 text-start">By : {author}</p>
+          <p className="mb-5 text-start">By : {author}</p>
           <div className="flex justify-between items-center mt-10 ">
             <div>
               <h1>{category}</h1>
