81d5204be81611fc27c85d71bb047172cf05d815
rubaiiyat <abirr1516@gmail.com>
2025-01-03 23:01:22 +0600

    change class to className at Banner component

--- src/Components/Banner/Banner.jsx
+++ src/Components/Banner/Banner.jsx
@@ -4,10 +4,10 @@ import { Link } from "react-router-dom";
 const Banner = () => {
   return (
     <div>
-      <div class="bg-base-200 text-white py-16 px-8 font-sans rounded-2xl mt-5">
-        <div class="container mx-auto flex flex-col md:flex-row justify-evenly  items-center gap-x-6 gap-y-8">
-          <div class="md:w-1/2 text-center md:text-left">
-            <h2 class="text-3xl md:text-2xl lg:text-5xl font-semibold mb-4">
+      <div className="bg-base-200 text-white py-16 px-8 font-sans rounded-2xl mt-5">
+        <div className="container mx-auto flex flex-col md:flex-row justify-evenly  items-center gap-x-6 gap-y-8">
+          <div className="md:w-1/2 text-center md:text-left">
+            <h2 className="text-3xl md:text-2xl lg:text-5xl font-semibold mb-4">
               Books to freshen up your bookshelf
             </h2>
             <Link to={"/listed"}>
@@ -15,7 +15,7 @@ const Banner = () => {
             </Link>
           </div>
 
-          <div class=" flex justify-center">
+          <div className=" flex justify-center">
             <img
               className="object-cover w-full md:w-9/12"
               src={banner}
