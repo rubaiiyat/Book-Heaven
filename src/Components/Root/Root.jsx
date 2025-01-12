a49c70d044729419e3a5d3430caae08c0b920fab
rubaiiyat <abirr1516@gmail.com>
2025-01-12 18:01:17 +0600

    flex grow the footer

--- src/Components/Root/Root.jsx
+++ src/Components/Root/Root.jsx
@@ -4,14 +4,16 @@ import { Outlet } from "react-router-dom";
 import Footer from "../Footer/Footer";
 const Root = () => {
   return (
-    <div>
+    <div className="flex flex-col min-h-screen">
       <div className="mx-0 md:mx-36">
         <Header></Header>
       </div>
-      <div className="mx-5 md:mx-40">
+      <div className="flex-grow mx-5 md:mx-40">
         <Outlet></Outlet>
       </div>
-      <Footer></Footer>
+      <div className="m-auto w-full">
+        <Footer></Footer>
+      </div>
     </div>
   );
 };
